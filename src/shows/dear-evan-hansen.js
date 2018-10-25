8527b3ddaa5b30afd1bf1bddc4262fbbfaef8e0f
Leo Lou <qibinlou@hotmail.com>
2018-10-24 22:36:00 -0400

    Combine log and Telegram notify; add more verbose logs.

--- src/shows/dear-evan-hansen.js
+++ src/shows/dear-evan-hansen.js
@@ -17,12 +17,20 @@ function isLotteryAvailaible(content) {
     return content.includes(UNAVAILAIBLE_MESSAGE);
 }
 
+function logAndNotify(message) {
+    console.log(message);
+    TelegramMessenger.sendMessage(message);
+}
+
 (async () => {
     const browser = await puppeteer.launch({
         headless: getPropertyOrDefault('HEADLESS') === 'true',
         args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
     let page = await browser.newPage();
+
+    logAndNotify('Ready to start for dear-evan-hansen!');
+
     await page.goto('https://my.socialtoaster.com/st/campaign_landing/?key=dearevanhansenlottery&source=iframe', {waitUntil: 'networkidle2'});
     page.click('#st_sign_in');
     await page.waitFor(2000);
@@ -35,26 +43,26 @@ function isLotteryAvailaible(content) {
     await fbLogin(fbLoginPage);
 
     await page.waitFor(2000);
+
+    logAndNotify('Successfully login with FB account.');
+
     const content = await page.$eval('#st_campaign_content', el => el.innerText);
     if (isLotteryAvailaible(content)) {
-        console.warn('Lottery is not available!');
-        TelegramMessenger.sendMessage(`${new Date().toString()} Lottery is not available!`);
+        logAndNotify('Lottery is not available!');
         await browser.close();
         return;
     }
 
     const lotteryButtons = await page.$$('.lottery_show_button .st_campaign_button');
     if (!lotteryButtons || lotteryButtons.length === 0) {
-        console.log('You may already checked in for the lottery today!');
-        TelegramMessenger.sendMessage(`${new Date().toString()} 'You may already checked in for the lottery today!'`);
+        logAndNotify('You may already checked in for the lottery today!');
         await browser.close();
         return;
     }
     for (let button of lotteryButtons) {
         button.click();
     }
-    console.log('Successfully checked in for today\'s lottery');
-    TelegramMessenger.sendMessage(`${new Date().toString()} Successfully checked in for today's lottery`);
+    logAndNotify('Successfully checked in for today\'s lottery');
 
     await browser.close();
 })();
